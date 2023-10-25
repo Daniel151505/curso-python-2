1cbb668b618f853245ff697e81822492eac0acf3
Daniel151505 <daniel150520011@gmail.com>
2023-10-24 22:22:43 -0500

    Primer programa

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hola, esto es Python")
+print("Hola soy Jhon Connor")
+
+# operaciones...
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# esto es un comentario
+
+""""
+Varias lineas
+otra linea
+"""
+
+'''
+Varias lineas
+'''
