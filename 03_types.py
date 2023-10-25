646433613af87ac29eb48b47071035d607bf822f
Daniel151505 <daniel150520011@gmail.com>
2023-10-24 23:05:31 -0500

    Tipos de datos

---
+++ 03_types.py
@@ -0,0 +1,22 @@
+# string
+my_name = "Nicolas"
+my_name = "Connor"
+my_name = "12"
+
+print('my_name ==> ', my_name)
+print(type(my_name))
+
+# int
+my_age = 45
+print('my_age ==> ', my_age)
+print(type(my_age))
+
+#boolean
+is_single = True
+print('is_single ==>', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('¿Cuál es tu edad?')
+print('my_age ==>', my_age)
+print(type(my_age))
