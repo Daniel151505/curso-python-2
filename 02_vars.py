ffc9235c4de46cea8a501b5d67e2f8333921f6ab
Daniel151505 <daniel150520011@gmail.com>
2023-10-24 22:40:26 -0500

    Variables

---
+++ 02_vars.py
@@ -0,0 +1,17 @@
+print("Hola, vars")
+
+# Esto es una variable
+my_name = "Jhon"
+print(my_name)
+
+#Esto es una variable con un número
+my_age = 12
+print(my_age)
+
+my_name = "Connor"
+print("aqui cambio", my_name)
+
+# input
+
+my_name = input("¿Cuál es tu nombre? ")
+print("Usando input", my_name)
